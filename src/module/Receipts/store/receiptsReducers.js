1affbf45a16eb2bd6d9c39be2868862671b02ada
Sindhu Selvaraj <sindhuselvaraj@zealeye.com>
2024-01-03 19:06:42 +0530

    Fetch data from table and form

--- src/module/Receipts/store/receiptsReducers.js
+++ src/module/Receipts/store/receiptsReducers.js
@@ -16,105 +16,105 @@ const initialState = {
             amount: "500.37",
             action: "Action",
           },
-          {
-            id: 2,
-            receiptNumber: "01Rep012302",
-            transactionCode: "123456",
-            transactionNumber: "Transac00123",
-            name: "Name",
-            customerCode: "CC0102",
-            date: "11/12/2023",
-            amount: "500.37",
-            action: "Action",
-          },
-          {
-            id: 3,
-            receiptNumber: "01Rep012302",
-            transactionCode: "123456",
-            transactionNumber: "Transac00123",
-            name: "Name",
-            customerCode: "CC0102",
-            date: "11/12/2023",
-            amount: "500.37",
-            action: "Action",
-          },
-          {
-            id: 4,
-            receiptNumber: "01Rep012303",
-            transactionCode: "123457",
-            transactionNumber: "Transac00124",
-            name: "Name",
-            customerCode: "CC0102",
-            date: "11/12/2023",
-            amount: "500.37",
-            action: "Action",
-          },
-          {
-            id: 5,
-            receiptNumber: "01Rep012307",
-            transactionCode: "123456",
-            transactionNumber: "Transac00129",
-            name: "Name",
-            customerCode: "CC0102",
-            date: "11/12/2023",
-            amount: "500.37",
-            action: "Action",
-          },
-          {
-            id: 6,
-            receiptNumber: "01Rep012307",
-            transactionCode: "123456",
-            transactionNumber: "Transac00123",
-            name: "Name",
-            customerCode: "CC0102",
-            date: "11/12/2023",
-            amount: "500.37",
-            action: "Action",
-          },
-          {
-            id: 7,
-            receiptNumber: "01Rep012302",
-            transactionCode: "123456",
-            transactionNumber: "Transac00123",
-            name: "Name",
-            customerCode: "CC0102",
-            date: "11/12/2023",
-            amount: "500.37",
-            action: "Action",
-          },
-          {
-            id: 8,
-            receiptNumber: "01Rep012301",
-            transactionCode: "123456",
-            transactionNumber: "Transac00123",
-            name: "Name",
-            customerCode: "CC0102",
-            date: "11/12/2023",
-            amount: "500.37",
-            action: "Action",
-          },
-          {
-            id: 9,
-            receiptNumber: "01Rep012302",
-            transactionCode: "123456",
-            transactionNumber: "Transac00123",
-            name: "Name",
-            customerCode: "CC0102",
-            date: "11/12/2023",
-            amount: "500.37",
-            action: "Action",
-          },
-          {
-            id: 10,
-            receiptNumber: "01Rep012302",
-            transactionCode: "123456",
-            transactionNumber: "Transac00123",
-            name: "Name",
-            customerCode: "CC0102",
-            date: "11/12/2023",
-            amount: "500.37",
-            action: "Action",
-          },
+          // {
+          //   id: 2,
+          //   receiptNumber: "01Rep012302",
+          //   transactionCode: "123456",
+          //   transactionNumber: "Transac00123",
+          //   name: "Name",
+          //   customerCode: "CC0102",
+          //   date: "11/12/2023",
+          //   amount: "500.37",
+          //   action: "Action",
+          // },
+          // {
+          //   id: 3,
+          //   receiptNumber: "01Rep012302",
+          //   transactionCode: "123456",
+          //   transactionNumber: "Transac00123",
+          //   name: "Name",
+          //   customerCode: "CC0102",
+          //   date: "11/12/2023",
+          //   amount: "500.37",
+          //   action: "Action",
+          // },
+          // {
+          //   id: 4,
+          //   receiptNumber: "01Rep012303",
+          //   transactionCode: "123457",
+          //   transactionNumber: "Transac00124",
+          //   name: "Name",
+          //   customerCode: "CC0102",
+          //   date: "11/12/2023",
+          //   amount: "500.37",
+          //   action: "Action",
+          // },
+          // {
+          //   id: 5,
+          //   receiptNumber: "01Rep012307",
+          //   transactionCode: "123456",
+          //   transactionNumber: "Transac00129",
+          //   name: "Name",
+          //   customerCode: "CC0102",
+          //   date: "11/12/2023",
+          //   amount: "500.37",
+          //   action: "Action",
+          // },
+          // {
+          //   id: 6,
+          //   receiptNumber: "01Rep012307",
+          //   transactionCode: "123456",
+          //   transactionNumber: "Transac00123",
+          //   name: "Name",
+          //   customerCode: "CC0102",
+          //   date: "11/12/2023",
+          //   amount: "500.37",
+          //   action: "Action",
+          // },
+          // {
+          //   id: 7,
+          //   receiptNumber: "01Rep012302",
+          //   transactionCode: "123456",
+          //   transactionNumber: "Transac00123",
+          //   name: "Name",
+          //   customerCode: "CC0102",
+          //   date: "11/12/2023",
+          //   amount: "500.37",
+          //   action: "Action",
+          // },
+          // {
+          //   id: 8,
+          //   receiptNumber: "01Rep012301",
+          //   transactionCode: "123456",
+          //   transactionNumber: "Transac00123",
+          //   name: "Name",
+          //   customerCode: "CC0102",
+          //   date: "11/12/2023",
+          //   amount: "500.37",
+          //   action: "Action",
+          // },
+          // {
+          //   id: 9,
+          //   receiptNumber: "01Rep012302",
+          //   transactionCode: "123456",
+          //   transactionNumber: "Transac00123",
+          //   name: "Name",
+          //   customerCode: "CC0102",
+          //   date: "11/12/2023",
+          //   amount: "500.37",
+          //   action: "Action",
+          // },
+          // {
+          //   id: 10,
+          //   receiptNumber: "01Rep012302",
+          //   transactionCode: "123456",
+          //   transactionNumber: "Transac00123",
+          //   name: "Name",
+          //   customerCode: "CC0102",
+          //   date: "11/12/2023",
+          //   amount: "500.37",
+          //   action: "Action",
+          // },
     ]
  
 };
@@ -168,25 +168,21 @@ const receiptsReducer = createSlice({
               state.error = typeof action.payload === "string" ? action.payload : "";
             }
           );
+        
           builder.addCase(postAddReceiptsMiddleware.pending, (state) => {
             state.loading = true;
           });
-          builder.addCase(
-            postAddReceiptsMiddleware.fulfilled,
-            (state, action) => {
-              state.loading = false;
-              state.BankDetailView = action.payload;
-            }
-          );
-          builder.addCase(
-            postAddReceiptsMiddleware.rejected,
-            (state, action) => {
-              state.loading = false;
-      
-              state.postAddReceiptsMiddleware = {};
-              state.error = typeof action.payload === "string" ? action.payload : "";
-            }
-          );
+          builder.addCase(postAddReceiptsMiddleware.fulfilled, (state, action) => {
+            console.log(action.payload,'find action.payload')
+            state.loading = false;
+            state.receiptsTableList = [...state.receiptsTableList, action.payload];
+          });
+          builder.addCase(postAddReceiptsMiddleware.rejected, (state, action) => {
+            state.loading = false;
+       
+            //   state.paymentVocherList = state.paymentVocherList;
+            state.error = typeof action.payload === "string" ? action.payload : "";
+          });
 
           builder.addCase(postPaymentDetailsMiddleware.pending, (state) => {
             state.loading = true;

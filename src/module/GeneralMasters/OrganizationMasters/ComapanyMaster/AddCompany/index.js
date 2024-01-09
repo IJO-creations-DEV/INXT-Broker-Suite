7b8bdde4857974ee594469d8ca66d77b30291a68
AyishaSidviqua <ayisha.siddika@zealeye.com>
2024-01-09 15:00:23 +0530

    organization company and branch integartion completed

--- src/module/GeneralMasters/OrganizationMasters/ComapanyMaster/AddCompany/index.js
+++ src/module/GeneralMasters/OrganizationMasters/ComapanyMaster/AddCompany/index.js
@@ -52,7 +52,7 @@ function AddCompany({ action }) {
       };
     }
   );
-  console.log(companyView, "companyView");
+  console.log(companyView.CompanyName, "companyView");
   const toastRef = useRef(null);
   const { id } = useParams();
   const [date, setDate] = useState(null);
